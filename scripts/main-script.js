c0d6a07e005af0ea7f3a28da359f3f4042e10bf2
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-03-21 22:22:59 -0600

    Create main-script.js

---
+++ scripts/main-script.js
@@ -0,0 +1,18 @@
+//capacitor
+
+class capacitor {
+  constructor() {
+    alert("the capacitor mod is added sucesfully")
+  }
+  capacitanceadd(valuestore) {
+    const store = valuestore
+  }
+  capacitanceget() {
+    return store
+    const store = ""
+  }
+  capacitanceerase() {
+    const store = ""
+  }
+  //BERD WOZ HERE IM SORRY FOR SAYING THAT YOUR LANGUAGE IS CRAP AND IT DOES NOT EXIST
+}
