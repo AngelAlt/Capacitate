340340abcd23b2ae39f060f6b65d3adcfffbf6d2
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-04-14 18:10:38 -0600

    Update Nizzle.js
    Nizzle.js is now an exportive file

--- scripts/Nizzle.js
+++ scripts/Nizzle.js
@@ -1,8 +1,8 @@
 //nizzle
 
-function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
+export function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
-function charge(i) {
+export function charge(i) {
   alert(["Charging for ",i," Seconds"])
   sleep((i * 1000)).then(() => {
     alert("D0ne")
@@ -10,7 +10,7 @@ function charge(i) {
   return i
 }
 
-function nozzle(hit) {
+export function nozzle(hit) {
   charge(hit)
   alert("Diss")
 }
